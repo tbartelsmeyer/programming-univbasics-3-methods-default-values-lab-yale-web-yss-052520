0d0af4daafc68595493a58a55f563628b5241116
Tilman Bartelsmeyer <tilman.bartelsmeyer@yale.edu>
2020-05-17 00:30:13 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -6,4 +6,5 @@ def meal_choice(veg1, veg2, protein = "tofu")
   p "A plate of #{protein} with #{veg1} and #{veg2}."
 end
 
-meal_choice("broccoli", "macaroni")
+return_val = meal_choice("broccoli", "macaroni")
+puts return_val
